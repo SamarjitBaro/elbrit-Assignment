434a1408436f42471e390a7d041d65b19fe54834
Samarjit Baro <xamarbro10@gmail.com>
2024-11-25 16:12:25 -0800

    responsive

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -23,12 +23,12 @@ export const Footer = () => {
     <div>
       <div className="bg-[#17414F] flex flex-col items-center justify-center min-h-[20vw] w-[100%] footer">
         <div className=" w-[80%]  md:px-16 md:min-h-[20vw]  py-6 ">
-          <div className="flex justify-center  items-center gap-1 mb-10 md:gap-12">
+          <div className="flex justify-center flex-wrap  md:flex-nowrap items-center gap-4 mb-10 md:gap-12">
             {items.map((item, index) => {
               return (
                 <div
                   key={index}
-                  className=" flex  bg-[#215D72] rounded md:rounded-lg md:h-[5vw] md:w-[22vw] w-[240px]  h-[30px]  justify-evenly items-center"
+                  className=" flex  bg-[#215D72] rounded md:rounded-lg md:h-[5vw] md:w-[22vw] w-[200px]  h-[36px]  md:justify-evenly  px-5 md:px-0 items-center"
                 >
                   <Image
                     alt="icons"
@@ -38,11 +38,11 @@ export const Footer = () => {
                     quality={100}
                     className="h-[3.2vw]  w-[3.2vw]"
                   />
-                  <div className="flex-col">
-                    <h2 className="text-[#FFFFFF] md:text-[1vw] text-[7px] font-normal">
+                  <div className="flex-col md:ml-0 ml-7">
+                    <h2 className="text-[#FFFFFF]  md:text-[1vw] text-[9px] font-normal">
                       {item.tag}
                     </h2>
-                    <h2 className="text-[#FFFFFF] md:text-[1vw] text-[5.7px]  font-medium">
+                    <h2 className="text-[#FFFFFF] md:text-[1vw] text-[7.7px]  font-medium">
                       {item.details}
                     </h2>
                   </div>
@@ -61,7 +61,7 @@ export const Footer = () => {
                 className="h-[4.6vw] w-[250px]"
               />
               <div className=" w-[300px]">
-                <p className="text-[#FFFF]  md:text-[0.8vw] text-[5.7px]  leading-1 md:leading-4 font-normal">
+                <p className="text-[#FFFF]  md:text-[0.8vw] text-[8.7px]  leading-1 md:leading-4 font-normal">
                   Your health, physical and emotional well-being is important to
                   us. We are always by your side and have made it even easier
                   for you to find the necessary vitamins.
@@ -80,7 +80,7 @@ export const Footer = () => {
                 className="md:h-[0.9vw] h-[7px] w-[7px] md:w-[10px]"
               />
             </div>
-            <p className="text-[#FFFF] ml-3 md:text-[0.8vw] text-[5.8px]  leading-1 md:leading-5 font-normal">
+            <p className="text-[#FFFF] ml-3 md:text-[0.8vw] text-[7.8px]  leading-1 md:leading-5 font-normal">
               Elbrit Life Sciences Private Limited. C20, BKC, G Block, Mumbai
               400051
             </p>
